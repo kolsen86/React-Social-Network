0f27ef98cd28aca0d74e4170fddfdb5fd7e7022e
Kenneth Olsen <olsenkenneth1986@gmail.com>
2019-08-01 21:12:32 -0400

    Added error handlers and authorization

---
+++ server/routes/user.routes.js
@@ -0,0 +1,26 @@
+import express from "express";
+import userCtrl from "../controllers/user.controller";
+
+const router = express.Router()
+
+router.route("/api/users")
+    .get(userCtrl.list)
+    .post(userCtrl.create)
+
+    router.route("/api/users").post(userCtrl.create)
+    router.route("/api/users").get(userCtrl.list)
+
+router.route("/api/users/:userId")
+    .get(userCtrl.read)
+    .put(userCtrl.update)
+    .delete(userCtrl.remove)
+
+    router.route("/api/users/:userId").get(userCtrl.read)
+    router.route("/api/users/:userId").put(userCtrl.update)
+    router.route("/api/users/:userId").delete(userCtrl.remove)
+
+router.param("userId", userCtrl.userById)
+router.param("userId", userCtrl.userById)
+
+
+export default router
